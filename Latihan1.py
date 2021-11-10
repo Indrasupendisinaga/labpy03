7ee6f3697617e9350ddf93ab64bb8eee0714c8fd
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-10 09:40:15 +0700

    penambahan sourcescode

--- Latihan1.py
+++ Latihan1.py
@@ -1,4 +1,13 @@
 print ("------------------")
 print ("Pertemuan ke 7")
-print ("latihan1")
+print ("latihan 1")
 print ("------------------")
+
+print ("menampilkan n bilangan acak yang lebih kecil dari 0.5")
+
+jumlah = int (input("masukkan nilai n :"))
+import random
+for i in range (jumlah) :
+
+    print ("data ke ", 1+i,"=>",(random.uniform(0.1,0.5))) 
+    
--- Latihan2.py
+++ Latihan2.py
@@ -2,3 +2,14 @@ print ("------------------")
 print ("Pertemuan ke 7")
 print ("latihan 2")
 print ("------------------")
+
+print ("menampilkan bilangan terbesar dari N buah data yang diinputkan")
+
+max = 0
+while True:
+    a=int(input("masukkan bilangan :"))
+    if max < a :
+        max=a
+    if a ==0:
+        break
+print ("\n bilangan terbesar adalah:", max)
