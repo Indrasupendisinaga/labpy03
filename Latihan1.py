cb7bfbb0bd6d7617617fe7d7c4efdd20c8c73d43
Indra Supendi Sinaga <92582081+Indrasupendisinaga@users.noreply.github.com>
2021-11-18 16:42:07 +0700

    Update Latihan1.py

--- Latihan1.py
+++ Latihan1.py
@@ -1,13 +1,15 @@
-print ("------------------")
-print ("Pertemuan ke 7")
-print ("latihan 1")
-print ("------------------")
+# latiha 1
+- sourcecode
 
-print ("menampilkan n bilangan acak yang lebih kecil dari 0.5")
-
-jumlah = int (input("masukkan nilai n :"))
-import random
-for i in range (jumlah) :
-
-    print ("data ke ", 1+i,"=>",(random.uniform(0.1,0.5))) 
+<p> print ("------------------")
+<p> print ("Pertemuan ke 7")
+<p> print ("latihan 1")
+<p> print ("------------------")
+<p> 
+<p> print ("menampilkan n bilangan acak yang lebih kecil dari 0.5")
+<p> 
+<p> jumlah = int (input("masukkan nilai n :"))
+<p> import random
+<p> for i in range (jumlah) :
+<p>     print ("data ke ", 1+i,"=>",(random.uniform(0.1,0.5))) 
     
